ed6648ec772d3eca0e195f6cb39927bc5af78614
Dylan D'Rozario <ddrozario@ddrozari-ltmt391.internal.salesforce.com>
2026-06-16 09:42:28 -0700

    Render fix attempt 1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 4173,
+    allowedHosts: ['all'],
+  },
 })
